15ca03b9b013a0d4afd4666b6aa9d9a56e51e349
Brennon Nuckols <brennuck@gmail.com>
2020-09-21 19:42:33 -0600

    login and register rendering and working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
-
+        <AmplifySignOut />
+        My App
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
