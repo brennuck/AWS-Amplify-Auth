b8c5753db63b1cb60440cb9eca3c3fb478c27da1
Brennon Nuckols <brennuck@gmail.com>
2020-09-24 09:55:32 -0600

    data rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { API, graphqlOperation } from "aws-amplify";
 
+import axios from "axios";
+import { v1 as uuidv1 } from "uuid";
+
 const listTodos = `query listTodos {
     listTodos{
       items{
@@ -24,9 +27,22 @@ const addTodo = `mutation createTodo($name:String! $description: String!) {
   }`;
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			bookshelf: [
+				{
+					bookId: uuidv1(),
+					author: "",
+					bookName: "",
+				},
+			],
+		};
+	}
+
 	todoMutation = async () => {
 		const todoDetails = {
-			name: "Party tonight!",
+			name: "Does this work? ",
 			description: "Amplify CLI rocks!",
 		};
 		const newTodo = await API.graphql(
@@ -40,13 +56,39 @@ class App extends React.Component {
 		const allTodos = await API.graphql(graphqlOperation(listTodos));
 		alert(JSON.stringify(allTodos));
 	};
+
+	componentDidMount() {
+		axios
+			.get(
+				"https://1alvwtvqf3.execute-api.us-west-2.amazonaws.com/default/GetMyBooks",
+				{
+					headers: {
+						"Content-Type": "application/json",
+						"x-api-key": "pXxRq2vdxi5ULGlmadMcP19aJgbQuDRq5WLPjjaT",
+					},
+				}
+			)
+			.then((res) => {
+				console.log(res);
+				this.setState({ bookshelf: res.data });
+				console.log("STAATE", this.state);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 	render() {
 		return (
 			<div className="App">
-				<p> Click a button </p>
-				<button onClick={this.listQuery}>GraphQL Query</button>
-				<button onClick={this.todoMutation}>GraphQL Mutation</button>
-                <AmplifySignOut />
+				{this.state.bookshelf.map((book) => {
+					return (
+						<div>
+							<div> {book.bookName} </div>
+							<div> {book.author} </div>
+						</div>
+					);
+				})}
+				<AmplifySignOut />
 			</div>
 		);
 	}
